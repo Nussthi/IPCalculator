ed992e6b830b43b98d432354ac0c0a5e540508aa
Thibaud <thibaud.nuss@gmail.com>
2023-03-17 14:31:14 +0100

    Adding a few lines to test the netID calculation

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from ip import IPAdress
 from baseconverter import Converter
+from ipcalculator import IPCalculator
 
 def main():
-    ip = IPAdress(127,730,120,90)
-    ip.display_decimal()
+    ip_adress = IPAdress(192,168,1,213)
+    mask_adress = IPAdress(255,255,255,0)
+    net_id_adress = IPAdress()
+    ip_calculator = IPCalculator()
 
-    base_converter = Converter()
-    print(base_converter.convert_to_binary(40))
+    print("\n----IP et masque----")
+    ip_adress.display_decimal()
+    mask_adress.display_decimal()
+    print("--------------------\n")
 
+    ip_adress.ip_to_binary()
+    mask_adress.ip_to_binary()
+    
+    print("----IP et Masque binaire----")
+    print(ip_adress.bytes_bin)
+    print(mask_adress.bytes_bin)
+    print("----------------------------\n")
+
+    net_id_adress = ip_calculator.get_netid(ip_adress,mask_adress)
+    net_id_adress.binary_ip_to_decimal()
+
+    print("----Net ID----")
+    net_id_adress.display_decimal()
+    print("--------------")
+    
 
 
 if __name__ == "__main__":
